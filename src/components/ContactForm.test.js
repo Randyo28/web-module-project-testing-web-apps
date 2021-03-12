8e708800091bff6f1b97a7d2c695100950faebf9
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-11 21:29:40 -0500

    Completed MVP

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
+import DisplayComponent from './DisplayComponent';
 
 
 test('renders without errors', ()=>{
@@ -86,13 +87,60 @@ test('renders "lastName is a required field" if an last name is not entered and 
 
     const lastNameError = await screen.findByText(/lastName is a required field/i)
     expect(lastNameError).toBeInTheDocument()
-    screen.debug()
+    // screen.debug()
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
+    render(<ContactForm/>);
+    
+    const firstNameInput = screen.getByLabelText('First Name*')
+    const lastNameInput = screen.getByLabelText('Last Name*')
+    const emailInput = screen.getByLabelText('Email*')
+    const button = screen.getByRole('button')
+    
+    
+    userEvent.type(firstNameInput, 'Randy')
+    userEvent.type(lastNameInput, 'Ortiz')
+    userEvent.type(emailInput, 'Randyortiz@email.com')
+    userEvent.click(button)
+
+
+
+    const firstSubmit = await screen.queryByTestId('firstnameDisplay')
+    const lastSubmit = await screen.queryByTestId('lastnameDisplay')
+    const emailSubmit = await screen.queryByTestId('emailDisplay')
+    
+    expect(firstSubmit).toBeVisible()
+    expect(lastSubmit).toBeVisible()
+    expect(emailSubmit).toBeVisible()
+    // screen.debug()
     
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
+    render(<ContactForm/>);
+
+    const firstNameInput = screen.getByLabelText('First Name*')
+    const lastNameInput = screen.getByLabelText('Last Name*')
+    const emailInput = screen.getByLabelText('Email*')
+    const messageInput = screen.getByLabelText('Message')
+    const button = screen.getByRole('button')
     
+    
+    userEvent.type(firstNameInput, 'Randy')
+    userEvent.type(lastNameInput, 'Ortiz')
+    userEvent.type(emailInput, 'Randyortiz@email.com')
+    userEvent.type(messageInput, 'Hello')
+    userEvent.click(button)
+
+    const firstSubmit = await screen.queryByTestId('firstnameDisplay')
+    const lastSubmit = await screen.queryByTestId('lastnameDisplay')
+    const emailSubmit = await screen.queryByTestId('emailDisplay')
+    const messageSubmit = await screen.queryByTestId('messageDisplay')
+    
+    expect(firstSubmit).toBeVisible()
+    expect(lastSubmit).toBeVisible()
+    expect(emailSubmit).toBeVisible()
+    expect(messageSubmit).toBeVisible()
+    screen.debug()
 });
